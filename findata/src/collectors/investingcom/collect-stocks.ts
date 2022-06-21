bc95e5ccc372e6fb8a74e0ba36cfb388166a697b
pods13 <alta0815@yandex.ru>
2022-06-21 12:05:30 +0200

    feat: collect stock data by country

--- findata/src/collectors/investingcom/collect-stocks.ts
+++ findata/src/collectors/investingcom/collect-stocks.ts
@@ -3,19 +3,21 @@ import { randomInteger } from '../../utils/random-int';
 import { CsvFormatterStream, format } from 'fast-csv';
 import fs from 'fs';
 
-export async function collectStocks() {
+export async function collectStocks(countryName: string) {
     const browser = await playwright.chromium.launch({
         headless: false, args: ['--single-process']
     });
 
     const csvStream = format({ headers: true });
-    const writeStream = fs.createWriteStream('./resources/stocks.csv');
+    const filename = `${countryName.replaceAll(' ', '-').toLowerCase()}.csv`;
+    const writeStream = fs.createWriteStream(`./resources/stocks/${filename}`);
     csvStream.pipe(writeStream).on('end', () => writeStream.end());
 
     const context = await browser.newContext();
     let pageNum = 0;
     try {
         const page = await openStockScreenerPage(context);
+        await selectCountry(page, countryName);
         let nextButtonVisible = await isNextButtonVisible(page);
         while (pageNum === 0 || nextButtonVisible) {
             pageNum += 1;
@@ -40,7 +42,7 @@ async function openStockScreenerPage(browserContext: playwright.BrowserContext):
     try {
         await page.goto('https://www.investing.com/stock-screener/?sp=country::27|sector::a|industry::a|equityType::a|exchange::a%3Ename_trans;1', {waitUntil: 'domcontentloaded'});
         // await adjustTableColumns(page);
-        await page.waitForTimeout(5000);
+        await page.waitForTimeout(3000);
         return page;
     } catch (error) {
         console.error(error);
@@ -55,6 +57,12 @@ async function adjustTableColumns(page: playwright.Page) {
     await page.click('input#SS_5');
     await page.click('input#SS_4');
     await page.click('#selectColumnsButton_stock_screener');
+}
+
+async function selectCountry(page: playwright.Page, countryName: string) {
+    await page.click('[placeholder="Select country"]');
+    await page.click(`#countriesUL > li:has-text("${countryName}")`);
+    await page.waitForTimeout(randomInteger(4000, 6000));
 }
 
 async function isNextButtonVisible(page: playwright.Page) {
@@ -87,4 +95,5 @@ async function selectNextPage(page: playwright.Page, pageToSelect: number) {
     return await page.click('.text_align_lang_base_2 > a:has-text("Next")');
 }
 
-collectStocks();
+collectStocks('Russia');
+collectStocks('Hong Kong');
