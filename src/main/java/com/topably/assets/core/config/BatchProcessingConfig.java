01032dab0b2f5513d791ce4e973491c0a31b8510
pods13 <alta0815@yandex.ru>
2022-07-12 18:37:20 +0200

    ref: remove spring batch from project

--- src/main/java/com/topably/assets/core/config/BatchProcessingConfig.java
+++
@@ -1,9 +0,0 @@
-package com.topably.assets.core.config;
-
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableBatchProcessing
-public class BatchProcessingConfig {
-}
