52e61d3b3ca52f1eed51fe4777ccfb00e4aecb58
pods13 <alta0815@yandex.ru>
2023-04-01 12:54:22 +0300

    fix: set allowedOrigins for websocket connection

--- src/main/java/com/topably/assets/core/config/WebSocketConfig.java
+++ src/main/java/com/topably/assets/core/config/WebSocketConfig.java
@@ -1,19 +1,25 @@
 package com.topably.assets.core.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${app.allowed-origins}")
+    private List<String> allowedOrigins;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/live")
-            .setAllowedOrigins("http://localhost:4200");
+            .setAllowedOrigins(allowedOrigins.toArray(new String[]{}));
     }
 
     @Override
