a17b7dc89572b7220b71ab958c8622192e09384c
pods13 <pods611@gmail.com>
2024-01-10 10:48:42 +0300

    ref: update testcontainers version

--- src/test/java/com/topably/assets/integration/base/IntegrationTestBase.java
+++ src/test/java/com/topably/assets/integration/base/IntegrationTestBase.java
@@ -1,25 +1,20 @@
 package com.topably.assets.integration.base;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import java.util.Map;
+
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.MySQLContainer;
 
-import java.util.Map;
 
 public abstract class IntegrationTestBase {
 
-    private static final MySQLContainer<?> container = new MySQLContainer<>("mysql:5.7.21")
+    @ServiceConnection
+    private static final MySQLContainer<?> database = new MySQLContainer<>("mysql:5.7.21")
         .withTmpFs(Map.of("/var/lib/mysql", "rw"))
-        .withCommand("--character-set-server=utf8 --collation-server=utf8_general_ci");
+        .withCommand("--character-set-server=utf8 --collation-server=utf8_general_ci")
+        .withUrlParam("useSSL", "false");
 
-    @BeforeAll
-    static void run() {
-        container.start();
-    }
-
-    @DynamicPropertySource
-    static void mysqlProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", () -> container.getJdbcUrl() + "?useSSL=false");
+    static {
+        database.start();
     }
 }
