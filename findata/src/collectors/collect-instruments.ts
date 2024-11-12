28fa09e1f0c6241ec4c16d9040d491e4971eae20
pods <pods611@gmail.com>
2024-11-12 10:10:29 +0300

    fix: collectors not invoked

--- findata/src/collectors/collect-instruments.ts
+++ findata/src/collectors/collect-instruments.ts
@@ -8,25 +8,27 @@ export async function collectInstruments(instrumentType: string, country: string
     collectedInstruments.pipe(fs.createWriteStream(composeFilename(`./resources/${instrumentType}s/${country}`)));
     extraInstruments.forEach(data => collectedInstruments.write(data));
     const pathToBaseInstruments = composeFilename(`${__dirname}/assets/${instrumentType}/${country}`);
-    try {
-        await fs.promises.access(pathToBaseInstruments, fs.constants.F_OK);
-        fs.createReadStream(pathToBaseInstruments)
-            .pipe(parse({headers: true}))
-            .on('error', error => console.error(error))
-            .on('data', (row: InstrumentData) => {
-                if (exchanges.length === 0 || exchanges.some((exchange) => exchange === row.exchange)) {
-                    const modifications = instrumentsToModify[`${row.symbol}.${row.exchange}`] ?? {};
-                    collectedInstruments.write({...row, ...modifications});
-                }
-            })
-            .on('end', (rowCount: number) => {
-                console.log(`Pulled from assets ${rowCount} ${instrumentType}`);
-                collectedInstruments.end();
-            });
-    } catch (err) {
-        collectedInstruments.end();
-    }
-    console.log(`Added ${extraInstruments.length} extra ${instrumentType}`);
+    return fs.promises.access(pathToBaseInstruments, fs.constants.F_OK)
+        .then(() => {
+            fs.createReadStream(pathToBaseInstruments)
+                .pipe(parse({headers: true}))
+                .on('error', error => console.error(error))
+                .on('data', (row: InstrumentData) => {
+                    if (exchanges.length === 0 || exchanges.some((exchange) => exchange === row.exchange)) {
+                        const modifications = instrumentsToModify[`${row.symbol}.${row.exchange}`] ?? {};
+                        collectedInstruments.write({...row, ...modifications});
+                    }
+                })
+                .on('end', (rowCount: number) => {
+                    console.log(`Pulled from assets ${rowCount} ${instrumentType}`);
+                    collectedInstruments.end();
+                });
+        })
+        .catch(err => {
+            collectedInstruments.end();
+        }).finally(() => {
+            console.log(`Added ${extraInstruments.length} extra ${instrumentType}`);
+        });
 }
 
 function composeFilename(subject: string) {
