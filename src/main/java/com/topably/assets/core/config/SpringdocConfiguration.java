4bba593befabbb03e99d3933620602df698d5e12
pods <pods611@gmail.com>
2025-07-01 10:21:27 +0300

    fix: add springdoc config to generate correct urls in swagger

---
+++ src/main/java/com/topably/assets/core/config/SpringdocConfiguration.java
@@ -0,0 +1,12 @@
+package com.topably.assets.core.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Server URL")})
+public class SpringdocConfiguration {
+}
